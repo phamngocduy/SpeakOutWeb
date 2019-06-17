ca8fe9aeba392ee483ce18dc12e591beeaf6abbd
phamngocduy <phamngocduy@DUY-PC>
2019-06-17 11:29:33 +0700

    Init project

---
+++ WebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SpeakOutWeb.MvcApplication" Language="C#" %>
---
+++ WebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SpeakOutWeb.Startup))]
+namespace SpeakOutWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
